498812f70f41fe1e5b658929db069664fc5a2184
jpearson <jpearson556@gmail.com>
2012-01-20 16:10:35 -0700

    Added devise users

---
+++ db/migrate/20120120225858_devise_create_users.rb
@@ -0,0 +1,16 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+      t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :time
+      t.timestamps
+    end
+
+    add_index :users, :email,                :unique => true
+    add_index :users, :reset_password_token, :unique => true
+  end
+
+end
